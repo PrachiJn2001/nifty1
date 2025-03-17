e466b6402a2abecf0fdfd530f6a0208c09443784
PrachiJn2001 <jainprachi821@gmail.com>
2025-03-17 15:35:13 +0530

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,1 +1,10 @@
+# dependencies.R
+list.of.packages <- c("shiny", "quantmod", "dplyr", "lubridate", "plotly")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
+library(shiny)
+library(quantmod)
+library(dplyr)
+library(lubridate)
+library(plotly)
